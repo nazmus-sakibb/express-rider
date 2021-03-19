45c71b8d3a045c8e8f83b81802146396d68ca5bf
NAZMUS SAKIB <sakibabir52@gmail.com>
2021-03-19 23:47:50 +0600

    Express Rider under constructions

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,10 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import firebase from "firebase/app";
 import "firebase/auth";
 import firebaseConfig from './firebase.config';
-import {UserContext} from "../../App";
+import { UserContext } from "../../App";
+import { useHistory, useLocation } from 'react-router';
+import './Login.css';
 
 if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
@@ -11,21 +13,26 @@ if (!firebase.apps.length) {
 }
 
 
-
-
-
 const Login = () => {
     const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+    const [user, setUser] = useState({});
+    
+
+    const history = useHistory();
+    const location = useLocation();
+
+    const { from } = location.state || { from: { pathname: "/" } };
 
     const handleGoogleSignIn = () => {
         const provider = new firebase.auth.GoogleAuthProvider();
-    
+
         firebase.auth()
             .signInWithPopup(provider)
             .then((result) => {
-                const {displayName, email} = result.user;
-                const signedInUser = {name: displayName, email};
+                const { displayName, email } = result.user;
+                const signedInUser = { name: displayName, email };
                 setLoggedInUser(signedInUser);
+                history.replace(from);
             }).catch((error) => {
                 // Handle Errors here.
                 var errorCode = error.code;
@@ -37,9 +44,48 @@ const Login = () => {
             });
     }
 
+    const handleSubmit = () => {
+
+    }
+
+    const handleBlur = (e) => {
+        let isFormValid = true;
+        if(e.target.name === 'email'){
+            isFormValid = /\S+@\S+\.\S+/.test(e.target.value);
+        }
+        if(e.target.name === 'password'){
+            const isPasswordValid = e.target.value.length > 6;
+            const passwordHasNumber = /\d{1}/.test(e.target.value);
+            isFormValid = isPasswordValid && passwordHasNumber;
+        }
+        if(isFormValid){
+            const newUser = {...user};
+            newUser[e.target.name] = e.target.value;
+            console.log(newUser);
+            setUser(newUser);
+        }
+    }
+
     return (
-        <div>
-            <h2>This is login page</h2>
+        <div className="text-center">
+            <div className="form-field">
+                <h2>Create an account</h2>
+                <p>Name: {user.name}</p>
+                <p>Email: {user.email}</p>
+                <p>Password: {user.password}</p>
+                <form onSubmit={handleSubmit}>
+                    <input onBlur={handleBlur} name="name" type="text" placeholder="Name" required />
+                    <br />
+                    <input onBlur={handleBlur} name="email" type="text" placeholder="Username or Email" required />
+                    <br />
+                    <input onBlur={handleBlur} type="password" name="password" id="" placeholder="Password" required />
+                    <br />
+                    {/* <input type="password" name="password" id="" placeholder="Confirm Password" required />
+                    <br/> */}
+                    <input type="submit" value="Create an account"/>
+                </form>
+            </div>
+            <hr/>
             <button onClick={handleGoogleSignIn}>Continue with Google</button>
         </div>
     );
