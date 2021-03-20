cac385684ef2b5dbed3b6be856607d3f56c60612
NAZMUS SAKIB <sakibabir52@gmail.com>
2021-03-21 01:21:23 +0600

    Intregate Google Map

--- src/components/Destination/Destination.js
+++ src/components/Destination/Destination.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import GoogleMap from '../GoogleMap/GoogleMap';
 import './Destination.css';
 
 const Destination = () => {
     return (
-        <div>
+        <div className="row">
             <div className="col-md-2 location-form">
                 <label for="pick-from">Pick From</label>
                 <input type="text" id="pick-from" placeholder="Farmgate"/>
@@ -15,7 +16,9 @@ const Destination = () => {
                 <br/>
                 <button>Search</button>
             </div>
-            <div className="col-md-10"></div>
+            <div className="col-md-10">
+                <GoogleMap/>
+            </div>
         </div>
     );
 };
